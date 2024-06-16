e6ccce46b092557c49c1f1c0339bec4696484492
Grimz <142171618+rendirenaldi09@users.noreply.github.com>
2024-06-16 09:30:11 +0700

    update

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -9,24 +9,24 @@ const Experience = () => {
       <Title text="Experiences" icon={<MdWork />} />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
         <ExperienceCard
-          title="ReactBD.com"
-          subTitle="Web Developer 2022 - Present"
+          title="PT Care Fast Indo"
+          subTitle="Cleaning Service 2019 - Present"
           icon={<SiReactivex />}
         />
         <ExperienceCard
-          title="Toptal"
-          subTitle="MERN Stack Developer 2021 - present"
+          title="PT Khong Guan"
+          subTitle="Production Biskuit 2020 - present"
           icon={<SiToptal />}
         />
         <ExperienceCard
-          title="Freelancer"
-          subTitle="MERN Stack Developer 2020 - 2021"
+          title="PT Bank Central Asia"
+          subTitle="Accounting Managemnt 2021 - 2022"
           icon={<SiFreelancer />}
         />
 
         <ExperienceCard
-          title="Fiverr"
-          subTitle="MERN Stack Developer 2019 - 2020"
+          title="PT Tri Adi Bersama"
+          subTitle="Stafff Operational 2022 - 2023"
           icon={<SiFiverr />}
         />
       </div>
