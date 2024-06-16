36ecd3629c65c1635ac00f0e516e98db62fa8312
Grimz <142171618+rendirenaldi09@users.noreply.github.com>
2024-06-16 11:22:39 +0700

    update

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,9 +1,8 @@
 import { FaProjectDiagram } from "react-icons/fa";
 import Title from "./Title";
-import amazonClone from "../public/img/projects/grimz.png";
-import cyberBlog from "../public/img/projects/github.png";
-import dynamicPortfolio from "../public/img/projects/reactjs.png";
-import dashboard from "../public/img/projects/dashboard.webp";
+import PortofolioGrimz from "../public/img/projects/grimz.png";
+import GithubProfile from "../public/img/projects/github.png";
+import ReactJS from "../public/img/projects/reactjs.png";
 import Image from "next/image";
 import ProjectCard from "./ProjectCard";
 
@@ -13,25 +12,20 @@ const Projects = () => {
       <Title text="Projects" icon={<FaProjectDiagram />} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 place-items-center">
         <ProjectCard
-          img={amazonClone.src}
+          img={PortofolioGrimz.src}
           title="Portofolio Grimz"
           link="https://rendirenaldi.vercel.app/"
         />     
         <ProjectCard
-          img={cyberBlog.src}
+          img={GithubProfile.src}
           title="Github Profile"
           link="https://github.com/rendirenaldi09"
         />       
         <ProjectCard
-          img={dynamicPortfolio.src}
+          img={ReactJS.src}
           title="ReactJS Portfolio"
           link="https://personal-portofolio-ashy.vercel.app/"
-        />
-        <ProjectCard
-          img={dashboard.src}
-          title="Dashboard"
-          link="https://orebishopping.reactbd.com/"
-        />
+        />        
       </div>
     </div>
   );
