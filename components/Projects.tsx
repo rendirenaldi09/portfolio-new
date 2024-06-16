3ebd98190078e2f3325f89f8259036120342ecee
Grimz <142171618+rendirenaldi09@users.noreply.github.com>
2024-06-16 11:13:39 +0700

    update

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,8 @@
 import { FaProjectDiagram } from "react-icons/fa";
 import Title from "./Title";
-import amazonClone from "../public/img/projects/amazonClone.webp";
+import amazonClone from "../public/img/projects/grimz.png";
 import cyberBlog from "../public/img/projects/github.png";
-import noorShop from "../public/img/projects/noorShop.webp";
 import dynamicPortfolio from "../public/img/projects/reactjs.png";
-import reactBD from "../public/img/projects/reactBD.png";
 import dashboard from "../public/img/projects/dashboard.webp";
 import Image from "next/image";
 import ProjectCard from "./ProjectCard";
@@ -16,30 +14,19 @@ const Projects = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 place-items-center">
         <ProjectCard
           img={amazonClone.src}
-          title="Amazon Clone"
-          link="https://amazonclone.reactbd.com/"
-        />
-        <ProjectCard
-          img={reactBD.src}
-          title="Cyber Security"
-          link="https://reactbd.com/"
-        />
+          title="Portofolio Grimz"
+          link="https://rendirenaldi.vercel.app/"
+        />     
         <ProjectCard
           img={cyberBlog.src}
           title="Github Profile"
           link="https://github.com/rendirenaldi09"
-        />
-        <ProjectCard
-          img={noorShop.src}
-          title="Noor Shopping"
-          link="https://orebishopping.reactbd.com/"
-        />
+        />       
         <ProjectCard
           img={dynamicPortfolio.src}
           title="ReactJS Portfolio"
           link="https://personal-portofolio-ashy.vercel.app/"
         />
-
         <ProjectCard
           img={dashboard.src}
           title="Dashboard"
